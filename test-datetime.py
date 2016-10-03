c53c42dac38cd9f7baf54461a58f2aa39bb104db
Jon Boydell <jonboydell@hotmail.com>
2016-10-03 17:06:56 +0100

    added delayed shooting

--- test-datetime.py
+++ test-datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import time
 from picamera import PiCamera
 
-target = datetime(2016, 10, 3, 16, 05)
+target = datetime(2016, 10, 3, 16, 8)
 now = datetime.now()
 cont = 1
 count = 100
@@ -26,4 +26,4 @@ while (cont > 0):
         for x in range(0, total_exposures):
             print 'photo {0}'.format(x)
             camera.capture('image%s.jpg' % x)
-            time.sleep(one_minute / exposures_per_minute)
+            time.sleep(minute_in_seconds / exposures_per_minute)
