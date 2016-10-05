ed6b20da77b19e07aa325f30af64cc7437759859
Jon Boydell <jonboydell@hotmail.com>
2016-10-05 12:56:39 +0100

    updated to work with time boundaries

--- test-datetime.py
+++ test-datetime.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import time
 from picamera import PiCamera
 
-target = datetime(2016, 10, 3, 16, 8)
+target = datetime(2016, 10, 3, 11, 0)
+end_target = datetime(2016, 10, 5, 12, 0)
 now = datetime.now()
 cont = 1
 count = 100
@@ -14,17 +15,13 @@ total_hours = 1
 hour_in_minutes = 60
 minute_in_seconds = 60
 exposures_per_minute = 8
+x = 0
 
 total_exposures = total_hours * hour_in_minutes * minute_in_seconds * exposures_per_minute
 
-while (cont > 0):
-    time.sleep(30)
+while (now > target && now < end_target):
     now = datetime.now()
-    print 'no'
-    if (now > target):
-        print 'yes'
-        cont = 0
-        for x in range(0, total_exposures):
-            print 'photo {0}'.format(x)
-            camera.capture('image%s.jpg' % x)
-            time.sleep(minute_in_seconds / exposures_per_minute)
+    print 'photo {0}'.format(x)
+    camera.capture('image%s.jpg' % x)
+    time.sleep(minute_in_seconds / exposures_per_minute)
+    x = x + 1
